696d7ec595a13c9566def435603793f4ebf84624
Berke Öz <beerkeozz@outlook.com>
2024-12-28 13:42:47 +0300

    Sipariş sayfasını oluşturdum ve sayfanın başlık bölümünün componentini ve css dosyasını oluşturup , sipariş verilecek sayfanın header bölümünü oluşturdum.

---
+++ src/components/OrderHeader.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import './OrderHeader.css';  
+
+const OrderHeader = () => {
+    return (
+        <header>
+            <img src="/images/iteration-1-images/logo.svg" alt="" />
+            
+        </header>
+    );
+};
+
+export default OrderHeader;  
---
+++ src/pages/OrderPage.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import OrderHeader from "../components/OrderHeader"; 
+
+const OrderPage = () => {
+    return (
+        <div>
+            <OrderHeader />
+        </div>
+    );
+};
+
+export default OrderPage;  
+
