665ce6bf825632a27d690130173a7b1188cd3dd1
Berke Öz <beerkeozz@outlook.com>
2024-12-28 14:19:14 +0300

    Değişiklikler kaydedildi

---
+++ src/components/PizzaOrderForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./PizzaOrderForm.css";
+
+function PizzaOrderForm() {
+    return (
+        <form>
+            <div className="header">
+                <h4>Position Absolute Pizza</h4>
+                <p>Dev olarak hala position: absolute kullanıyorsan bu çok acı bir pizza tam sana göre. 
+ 
+
+                </p>
+                
+            </div>
+        </form>
+    );
+}
+
+export default PizzaOrderForm;
--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,11 +1,14 @@
 
 import React from "react";
 import OrderHeader from "../components/OrderHeader"; 
+import PizzaOrderForm from "../components/PizzaOrderForm";
 
 const OrderPage = () => {
     return (
         <div>
             <OrderHeader />
+            <PizzaOrderForm />
+        
         </div>
     );
 };
